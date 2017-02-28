28e65bdd9b5f52cb997bebddfb1d519b452768c2
Kirill Shirokov <wide.cyril@gmail.com>
2017-02-28 15:32:53 +0300

    2
    без документации и тестов

--- stack.cpp
+++ stack.cpp
@@ -1,151 +1,149 @@
 #include "stack.h"
+#include <assert.h>
 
-#define equ(stream, item) stream << #item" = " << item << '\n'
-#define NOWSTACK(num)	logs_ << "now stack #" << num << " is\n";\
-						if (data_)\
-						{\
-							logs_ << "{ ";\
-							if (count_)\
-								logs_ << data_[0];\
-							for (int i = 1; i < count_; i++)\
-								logs_ << ", " << data_[i];\
-							logs_ << " }\n";\
-						}\
-						else\
-							logs_ << "NULL\n";\
-						equ(logs_,size_);\
-						equ(logs_,count_) << '\n';
-
-int stack::obj_counter = 0;
-std::ofstream logs_;
+#define ASSERT_OK()					\
+	if (!ok())						\
+	{								\
+		dump();						\
+		assert(!"stack is ok");		\
+	}
 
 stack::stack() : count_(0), size_(init_size)
 {
 	data_ = new value_type[init_size];
-	if (obj_counter == 0)
-		logs_.open("logs_.txt");
-	logs_ << "stack #" << (number = ++obj_counter) << " was created\n";
-	NOWSTACK(number);
 }
 
 stack::stack(size_t size) : count_(0), size_(size)
 {
 	data_ = new value_type[size];
-	if (obj_counter == 0)
-		logs_.open("logs_.txt");
-	logs_ << "stack #" << (number = ++obj_counter) << " was created\n";
-	NOWSTACK(number);
 }
 
 stack::stack(const stack& that): count_(that.count_), size_(that.size_)
 {
-	logs_ << "copy constructor was called\n";
 	data_ = new value_type[size_];
+	ASSERT_OK();
 	for (int i = 0; i < count_; i++)
 		data_[i] = that.data_[i];
-	logs_ << "stack #" << (number = ++obj_counter) << " was created\n";
-	NOWSTACK(number);
 }
 
-stack::stack(stack&& that) : count_(that.count_), size_(that.size_)
+stack::stack(stack&& that) : count_(that.count_), size_(that.size_), data_(that.data_)
 {
-	logs_ << "move constructor was called\n";
-	data_ =	that.data_;
+	ASSERT_OK();
 	that.data_ = NULL;
-	logs_ << "stack #" << (number = ++obj_counter) << " was created\n";
-	NOWSTACK(number);
 }
 
 stack::~stack()
 {
 	if (data_)
 		delete[] data_;
-	logs_ << "stack #" << number << " was deleted\n";
-	if(--obj_counter == 0)
-		logs_.close();
 }
 
 stack &stack::operator=(stack &another)
 {
-	logs_ << "operator=(copy) was called\n";
-	if (this != &another) {
+	ASSERT_OK();
+	if (this != &another)
+	{
 		delete[] data_;
 		size_ = another.size_;
 		count_ = another.count_;
 		data_ = new value_type[size_];
+		ASSERT_OK();
 		for (int i = 0; i < count_; i++)
 			data_[i] = another.data_[i];
-		NOWSTACK(number);
 	}
-	else
-		logs_ << "this == another\n";
+	ASSERT_OK();
 	return *this;
 }
 
 stack &stack::operator=(stack &&another)
 {
-	logs_ << "operator=(move) was called\n";
-	if (this != &another) {
+	ASSERT_OK();
+	if (this != &another)
+	{
 		delete[] data_;
 		size_ = another.size_;
 		count_ = another.count_;
 		data_ = another.data_;
 		another.data_ = NULL;
-		NOWSTACK(number);
 	}
-	else
-		logs_ << "this == another\n";
+	ASSERT_OK();
 	return *this;
 }
 
-bool stack::ok()
+bool stack::ok() const
 {
+	return !(count_ > size_ || data_ == NULL);
+}
+
+#define equ(item) _dump << "\t"#item" = " << item << '\n'
+#define equ2(item,index) _dump << "\t"#item"[" << index << "] = " << item[index] << '\n'
+
+bool stack::dump() const
+{
+	std::ofstream _dump("dump.txt");
+	if (_dump.fail())
+		return false;
+	_dump << "Stack\n{\n";
+	equ(size_);
+	equ(count_);
+	_dump << "\tdata_\n";
+	if (data_)
+	{
+		_dump << "\t{\n\t";
+		for (int i = 0; i < size_; i++)
+			equ2(data_,i) << '\t';
+		_dump << "}\n";
+	}
+	else
+		_dump << "\t\tis NULL\n";
+	_dump << '}';
+	_dump.close();
 	return true;
 }
 
-bool stack::dump()
-{
-	return true;
-}
+#undef equ
 
 bool stack::push(value_type elem)
 {
-	if (count_ == size_) {
-		data_ = (value_type*)realloc(data_, sizeof(value_type) * (size_ <<= 1));
-		logs_ << "size_ was changed\n \n";
+	ASSERT_OK();
+	if (count_ == size_)
+	{
+		value_type *temp = (value_type*)realloc(data_, sizeof(value_type) * (size_ << 1));
+		if (temp)
+		{
+			data_ = temp;
+			size_ <<= 1;
+		}
+		else
+			return false;
 	}
-	if (data_) {
-		data_[count_++] = elem;
-		logs_ << "new element was pushed\n";
-		NOWSTACK(number);
-		return 0;
-	}
-	logs_ << "data_ was NULL idk why\n\n";
-	return 1;
+	data_[count_++] = elem;
+	ASSERT_OK();
+	return true;
 }
 
 stack::value_type stack::pop()
 {
+	ASSERT_OK();
 	int value;
 	if (count_)
 		value = data_[--count_];
-	else {
-		logs_ << "count_ = 0, pop wasn't done\n\n";
-		
-	}
-	if (count_ <= size_ >> 1)
+	else
+		throw 42;
+	if (count_ <= size_ >> 1 && size_ >> 1 > 4)
 		data_ = (value_type*)realloc(data_, sizeof(value_type) * (size_ >>= 1));
-	logs_ << "top was deleted\n";
-	NOWSTACK(number);
+	ASSERT_OK();
 	return value;
 }
 
-size_t stack::size()
+size_t stack::size() const
 {
 	return size_;
 }
 
-size_t stack::length()
+size_t stack::length() const
 {
 	return count_;
 }
+
+#undef ASSERT_OK
--- stack.h
+++ stack.h
@@ -2,34 +2,31 @@
 
 #include <fstream>
 
-const size_t init_size = 16;			// size of data after construct
-
 class stack
 {
 public:
 	typedef int value_type;				// type in stack
 
 	stack();							// constructor
-	stack(size_t size);					// constructor too
+	stack(size_t);						// constructor too
 	stack(const stack &);				// copy constructor
 	stack(stack &&);					// move constructor
 	~stack();							// destructor
 
-	stack &operator=(stack &another);	// copy assignment
-	stack &operator=(stack &&another);	// moce assignment
+	stack &operator=(stack &);			// copy assignment
+	stack &operator=(stack &&);			// move assignment
 
-	bool ok();							// ok
-	bool dump();						// dump
+	bool ok() const;					// ok
+	bool dump() const;					// dump
 
 	bool push(value_type);				// push
 	value_type pop();					// pop
 
-	size_t size();						// size of data
-	size_t length();					// number of elements
-	
-	static int obj_counter;				// object counter
+	size_t size() const;				// size of data
+	size_t length() const;				// number of elements
+
 private:
-	int number;							// object number
+	static const size_t init_size = 4;	// default size of data
 
 	value_type *data_;					// data
 	size_t count_;						// counter for elements
